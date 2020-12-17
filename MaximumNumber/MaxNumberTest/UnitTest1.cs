570517c2c9fa35b47d0df3c5c6c2273bf8328ac6
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-17 16:55:44 +0530

    [Pratibha Mastud] Add: Find Max Number in between three float numbers

--- MaximumNumber/MaxNumberTest/UnitTest1.cs
+++ MaximumNumber/MaxNumberTest/UnitTest1.cs
@@ -77,38 +77,5 @@ namespace MaxNumberTest
             float result = MaximumNumbers.MaxFloatNumber(firstValue, secondValue, thirdValue);
             Assert.AreEqual(expectedResult, result);
         }
-
-        /// <summary>
-        /// Check Maximum String and Return Peach
-        /// </summary>
-        [TestMethod]
-        public void CheckFirstStringWhenGreaterReturnPeach()
-        {
-            string max = "Peach";
-            string Result = MaximumNumbers.StringMaximumNumber("Peach", "Apple", "Banana");
-            Assert.AreEqual(max, Result);
-        }
-
-        /// <summary>
-        /// Check MAximum String and Return Strawberry
-        /// </summary>
-        [TestMethod]
-        public void CheckSecondStringWhenGreaterRetrunStrawberry()
-        {
-            string max = "Strawberry";
-            string Result = MaximumNumbers.StringMaximumNumber("Apple", "Strawberry", "Peach");
-            Assert.AreEqual(max, Result);
-        }
-
-        /// <summary>
-        /// Check Maximum Sting And Return Peach
-        /// </summary>
-        [TestMethod]
-        public void CheckThirdStringWhenGreaterRetrunPeach()
-        {
-            string max = "Peach";
-            string Result = MaximumNumbers.StringMaximumNumber("Apple", "Banana", "Peach");
-            Assert.AreEqual(max, Result);
-        }
     }
 }
