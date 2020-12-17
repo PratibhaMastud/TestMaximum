1885c533c84c50adb90e2cdfc51d55dde0300d5e
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-17 11:10:45 +0530

    [Pratibha Mastud] Add: Find Max number between given three Float number

--- MaximumNumber/MaxNumberTest/UnitTest1.cs
+++ MaximumNumber/MaxNumberTest/UnitTest1.cs
@@ -42,5 +42,41 @@ namespace MaxNumberTest
             int result = MaximumNumbers.MaxIntNumber(firstValue, secondValue, thirdValue);
             Assert.AreEqual(expectedResult, result);
         }
+
+        /// <summary>
+        /// TC1.1 here we find max float number in between given three float number
+        /// <summary/>
+        [TestMethod]
+        [DataRow(66.3f, 5.3f, 4.2f)]
+        public void GivenThreeFloatWhenMaxFloatAtPositionOneShouldReturnThatValue(float firstValue, float secondValue, float thirdValue)
+        {
+            float expectedResult = firstValue;
+            float result = MaximumNumbers.MaxFloatNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expectedResult, result);
+        }
+        /// <summary>
+        /// TC1.2 here we find max float number in between given three float number
+        /// <summary/>
+        [TestMethod]
+        [DataRow(33.2f, 445.3f, 5.22f)]
+        public void GivenThreeFloatWhenMaxFloatAtPositionTwoShouldReturnThatValue(float firstValue, float secondValue, float thirdValue)
+        {
+            float expectedResult = secondValue;
+            float result = MaximumNumbers.MaxFloatNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        /// <summary>
+        /// TC1.3 here we find max float number in between given three float number
+        /// <summary/>
+        [TestMethod]
+        [DataRow(63.3f, 6.53f, 93.2f)]
+        public void GivenThreeFloatWhenMaxFloatAtPositionThreeShouldReturnThatValue(float firstValue, float secondValue, float thirdValue)
+        {
+            float expectedResult = thirdValue;
+            float result = MaximumNumbers.MaxFloatNumber(firstValue, secondValue, thirdValue);
+            Assert.AreEqual(expectedResult, result);
+        }
+
     }
 }
--- MaximumNumber/MaximumNumber/Program.cs
+++ MaximumNumber/MaximumNumber/Program.cs
@@ -6,10 +6,14 @@ namespace MaximumNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to this program where we will be finding maximum number using generics");
+            Console.WriteLine("Welcome in this program we finding maximum number using generics");
             Console.WriteLine(MaximumNumbers.MaxIntNumber(786, 60, 212));
             Console.WriteLine(MaximumNumbers.MaxIntNumber(70, 733, 212));
             Console.WriteLine(MaximumNumbers.MaxIntNumber(943, 73, 3312));
+            Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 6.5f, 2.2f));
+            Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.6f, 66.5f, 3f));
+            Console.WriteLine(MaximumNumbers.MaxFloatNumber(7.46f, 65.5f, 231.2f));
+
         }
     }
 }
